0d104f2352275e6e461c3ae317646b8e6482e76e
Samit Raj <sunnybhasneiya@gmail.com>
2026-05-21 18:12:30 +0530

    I have created a POST API through which products can be added.

--- product-service/server.js
+++ product-service/server.js
@@ -4,9 +4,9 @@ const app = express();
 require('dotenv').config();
 const port = 'process.env.PORT';
 const {productRouter} = require('./route/product');
-
+const config = require('./config/index')
 
 app.use(express.json())
 app.use('/', productRouter);
 
-app.listen(port, () => console.log(`Server is running on ${port}`))
+app.listen(config.PORT, () => console.log(`Server is running on ${config.PORT}`))
